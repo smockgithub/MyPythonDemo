e202c4c4efa7135c2b8316443e51bf0095d084d8
董煜 <32924305+smockgithub@users.noreply.github.com>
2020-03-06 23:00:02 +0800

    Update music163.py

--- Robot/music163.py
+++ Robot/music163.py
@@ -16,34 +16,34 @@
 # <span class="nb">17万</span>
 # </div>
 
-from selenium import webdriver
-import time
-import os
+from selenium import webdriver #导入浏览器模块
+import time #导入时间模块？好像没用用到
+import os #os是用于操作文件的模块
 
-url='https://music.163.com/#/discover/playlist/'
+url='https://music.163.com/#/discover/playlist/' #定义个变量url
 
-# 创建一个浏览器
-browser = webdriver.Chrome()
+browser = webdriver.Chrome() #启动一个浏览器窗口
 
+#定义一个path参数，用于指定最终的文件存储的路径
 path = os.environ['USERPROFILE']+"\\Documents\\python\\"
 if os.path.exists(path) == False :
     os.makedirs(path)
 
 while url != 'javascript:void(0)':
     print("url=",url)
-    browser.get(url)
+    browser.get(url) #打开某个网址
 
-    # time.sleep(1)
+    # time.sleep(1) #暂停1毫秒还是1秒
 
     # 切换到iframe,iframe name = contentFrame
-    browser.switch_to.frame("contentFrame")
+    browser.switch_to.frame("contentFrame") #看要抓取的网页是否需要切换，主要看要抓取的页面是否在frame里，99%的网页都没有frame的
 
-    # data = browser.find_element_by_id('m-pl-container').find_element_by_tag_name('li')
-    data = browser.find_element_by_id("m-pl-container").find_elements_by_tag_name("li")
-    # print(data[0].text)
-    # print(len(data))
+    #查找id=m-pl-container的元素，在这个元素里面查找所有<li>的元素，把查找到的所有li放到参数data里
+    data = browser.find_element_by_id("m-pl-container").find_elements_by_tag_name("li") 
+    
+    # print(data[0].text) #输出data的内容到控制台
 
-    for i in range(len(data)):
+    for i in range(len(data)): #循环每一个<li>元素
         num = data[i].find_element_by_class_name("nb").text
         if '万' in num and int(num.split('万')[0]) > 1000: # 大于1千万
             msk = data[i].find_element_by_css_selector("a.msk")
